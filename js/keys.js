fcdad9b898fff3bb1581975fcd11c8de8832ca44
Tim Acker <tjacker31@gmail.com>
2017-07-21 13:20:11 -0400

    Add files via upload
    Initial upload of project files.

---
+++ js/keys.js
@@ -0,0 +1,8 @@
+/*jslint esversion: 6, browser: true*/
+
+exports.twitterKeys = {
+  consumer_key: 'qWYKNipibIXPkwiTbjxnzttcJ',
+  consumer_secret: 'C6n2VSYwp4VW81njoCHB0Y26yGxEcKgBv6puAdJRHkK27Ds2b7',
+  access_token_key: '888435415459475457-cDGJUpAvLnaTIdqyqmS8B9bn2ciR9Ax',
+  access_token_secret: 'kPD0DIPUzyys3uoxy6qgrSmy0fDAvOKRd5LXuIkAF5A65',
+};
