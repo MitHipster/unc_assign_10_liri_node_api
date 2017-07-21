d72d96672ed40870eacef46aad7f3d47fba64a41
Tim Acker <tjacker31@gmail.com>
2017-07-21 16:31:34 -0400

    Twitter
    Using Twitter node, created function to pull last 20 tweets and when created from alias Twitter account.

--- js/twitter.js
+++ js/twitter.js
@@ -1,1 +1,31 @@
 /*jslint esversion: 6, browser: true*/
+const Twitter = require('twitter');
+const keys = require('./keys.js');
+const params = {
+  screen_name: 'BootyMcBootCamp',
+  count: 20
+};
+
+let request = new Twitter(keys.twitterKeys);
+
+function getTweets() {
+  request.get('statuses/user_timeline', params, function (error, tweets, response) {
+    if (error) {
+      console.error(error.message);
+    }
+    console.log(
+      '\n' +
+      'Screen Name: ' + tweets[0].user.screen_name
+    );
+
+    tweets.forEach(function (tweet) {
+      console.log(
+        '\n' +
+        'Tweet: ' + tweet.text + '\n' +
+        'Posted: ' + tweet.created_at + '\n'
+      );
+    });
+  });
+}
+
+module.exports.getTweets = getTweets;
--- liri.js
+++ liri.js
@@ -1,1 +1,13 @@
 /*jslint esversion: 6, browser: true*/
+
+const source = process.argv[2]; // Source for search request
+const searchTerm = process.argv[3]; // Search term used for source (search term not used twitter)
+const twitter = require('./js/twitter.js');
+
+switch (source) {
+  case 'my-tweets':
+    twitter.getTweets();
+    break;
+  default:
+    console.log('Not a valid source. Please try again.');
+}
