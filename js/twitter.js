993e1fc7f316e59823bfd7685e3d53eadfe9dbd7
Tim Acker <tjacker31@gmail.com>
2017-07-21 20:20:49 -0400

    Twitter Error Handler
    Added basic error handling for twitter feed.

--- js/twitter.js
+++ js/twitter.js
@@ -1,5 +1,6 @@
 /*jslint esversion: 6, browser: true*/
 const Twitter = require('twitter');
+const liri = require('../liri.js');
 const keys = require('./keys.js');
 const params = {
   screen_name: 'BootyMcBootCamp',
@@ -11,20 +12,21 @@ let request = new Twitter(keys.twitterKeys);
 function getTweets() {
   request.get('statuses/user_timeline', params, function (error, tweets, response) {
     if (error) {
-      console.error(error.message);
-    }
-    console.log(
-      '\n' +
-      'Screen Name: ' + tweets[0].user.screen_name
-    );
-
-    tweets.forEach(function (tweet) {
+      liri.printError(error[0]);
+    } else {
       console.log(
         '\n' +
-        'Tweet: ' + tweet.text + '\n' +
-        'Posted: ' + tweet.created_at + '\n'
+        'Screen Name: ' + tweets[0].user.screen_name
       );
-    });
+
+      tweets.forEach(function (tweet) {
+        console.log(
+          '\n' +
+          'Tweet: ' + tweet.text + '\n' +
+          'Posted: ' + tweet.created_at + '\n'
+        );
+      });
+    }
   });
 }
 
--- liri.js
+++ liri.js
@@ -4,6 +4,11 @@ const source = process.argv[2]; // Source for search request
 const searchTerm = process.argv[3]; // Search term used for source (search term not used twitter)
 const twitter = require('./js/twitter.js');
 
+// Function to console log error messages
+function printError(error) {
+  console.error(error.message);
+}
+
 switch (source) {
   case 'my-tweets':
     twitter.getTweets();
@@ -11,3 +16,5 @@ switch (source) {
   default:
     console.log('Not a valid source. Please try again.');
 }
+
+module.exports.printError = printError;
